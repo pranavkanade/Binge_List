2a498b8c0c680eeb6333f8126b9d4c663f5963e6
pskanade <k.pranav.apk@gmail.com>
2019-09-07 02:37:13 +0530

    Made the whole movie card clickable

--- src/components/ui/movieCard.js
+++ src/components/ui/movieCard.js
@@ -3,24 +3,24 @@ import Link from "next/link";
 import { Card, Icon, Image, Header } from "semantic-ui-react";
 
 const MovieCard = props => (
-  <Card fluid>
-    <Image
-      src={`https://image.tmdb.org/t/p/w300${props.movie.poster_path}`}
-      size="tiny"
-      style={{ height: "320px" }}
-      ui={false}
-    />
-    <Card.Content extra>
-      <Link
-        key={props.movie.id}
-        href="/movies/[movId]"
-        as={`/movies/${props.movie.id}`}>
+  <Link
+    key={props.movie.id}
+    href="/movies/[movId]"
+    as={`/movies/${props.movie.id}`}>
+    <Card fluid>
+      <Image
+        src={`https://image.tmdb.org/t/p/w300${props.movie.poster_path}`}
+        size="tiny"
+        style={{ height: "320px" }}
+        ui={false}
+      />
+      <Card.Content extra>
         <a>
-          <Header as="h4">{props.movie.original_title}</Header>
+          <Header as="h4">{props.movie.title}</Header>
         </a>
-      </Link>
-    </Card.Content>
-  </Card>
+      </Card.Content>
+    </Card>
+  </Link>
 );
 
 export default MovieCard;
