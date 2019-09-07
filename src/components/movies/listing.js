fd8813cf981175733ba262dad1a0f051d4110371
pskanade <k.pranav.apk@gmail.com>
2019-09-07 18:12:15 +0530

    Separated a generic movie listing component from popular.js to be reused

---
+++ src/components/movies/listing.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import { Segment, Message, Card } from "semantic-ui-react";
+import MovieCard from "../ui/movieCard";
+import Loader from "../ui/loader";
+import Pagination from "../ui/pagination";
+
+const NUM_OF_MOVIES_IN_A_ROW = 4;
+
+// This can be turned into functional component, by creating card grp UI component and pushing handlePagination to parent comonent.
+class MovieListing extends Component {
+  renderMovieCards = () => {
+    if (this.props.moviesList.length === 0) {
+      return (
+        <Message info>
+          <Message.Header>No more movies to show</Message.Header>
+        </Message>
+      );
+    }
+    const movieCards = this.props.moviesList.map(movie => {
+      return <MovieCard movie={movie} key={movie.id} />;
+    });
+
+    return (
+      <Card.Group itemsPerRow={NUM_OF_MOVIES_IN_A_ROW}>
+        {movieCards}
+      </Card.Group>
+    );
+  };
+
+  handlePaginationChange = async (e, { activePage }) => {
+    this.props.fetchMoviesFromPage(activePage);
+  };
+
+  render() {
+    return (
+      <Segment basic>
+        <Segment basic>
+          <Pagination
+            activePage={this.props.activePage}
+            handlePaginationChange={this.handlePaginationChange}
+            totalPages={this.props.totalPages}
+          />
+        </Segment>
+        {this.props.moviesList ? (
+          this.renderMovieCards()
+        ) : (
+          // If data is yet to be fetched; Show loader
+          <Loader />
+        )}
+        <Segment basic>
+          <Pagination
+            activePage={this.props.activePage}
+            handlePaginationChange={this.handlePaginationChange}
+            totalPages={this.props.totalPages}
+          />
+        </Segment>
+      </Segment>
+    );
+  }
+}
+
+export default MovieListing;
--- src/components/movies/popular.js
+++ src/components/movies/popular.js
@@ -1,78 +1,31 @@
 import React, { Component } from "react";
-import {
-  Button,
-  Segment,
-  Message,
-  Pagination,
-  Card,
-  Divider
-} from "semantic-ui-react";
 import { getPopularMovies } from "../../api/movies";
-import MovieCard from "../ui/movieCard";
-import Loader from "../ui/loader";
+import Listing from "./listing";
 
 class PopularMovies extends Component {
   state = {
-    popularMovies: null,
+    moviesList: null,
     activePage: 1,
     totalPages: 1
   };
 
-  renderMovieCards = () => {
-    if (this.state.popularMovies.length === 0) {
-      return (
-        <Message info>
-          <Message.Header>No more movies to show</Message.Header>
-        </Message>
-      );
-    }
-    const movieCards = this.state.popularMovies.map(movie => {
-      return <MovieCard movie={movie} key={movie.id} />;
-    });
-
-    return <>{movieCards}</>;
-  };
-
-  handlePaginationChange = async (e, { activePage }) => {
+  fetchMoviesFromPage = async activePage => {
     this.setState({ activePage });
     // Every time we change the page number the api will be invoked to find the listing on that page
     getPopularMovies(activePage, this.storePopularMovies);
   };
 
-  // Use the function both at the top and at the bottom so that it'll be convenient for the user
-  renderPagination = () => {
-    const { activePage, totalPages } = this.state;
-    return (
-      <Pagination
-        activePage={activePage}
-        onPageChange={this.handlePaginationChange}
-        size="medium"
-        totalPages={totalPages}
-      />
-    );
-  };
-
   render() {
     return (
-      <>
-        <Segment basic>
-          {this.renderPagination()}
-          <Divider hidden />
-          {this.state.popularMovies ? (
-            <Card.Group itemsPerRow={4}>{this.renderMovieCards()}</Card.Group>
-          ) : (
-            // If data is yet to be fetched; Show loader
-            <Loader />
-          )}
-          <Divider hidden />
-          {this.renderPagination()}
-        </Segment>
-      </>
+      <Listing
+        {...this.state}
+        fetchMoviesFromPage={this.fetchMoviesFromPage}
+      />
     );
   }
 
-  storePopularMovies = (popularMovies, totalPages) => {
-    this.setState({ popularMovies, totalPages });
+  storePopularMovies = (moviesList, totalPages) => {
+    this.setState({ moviesList, totalPages });
   };
 
   componentDidMount() {
