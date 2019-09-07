c316d639aa3b76234a84b192c03be0b4dfadfc95
pskanade <k.pranav.apk@gmail.com>
2019-09-08 00:14:23 +0530

    pushed search bar into layout

--- src/components/app.js
+++ src/components/app.js
@@ -1,40 +1,12 @@
 import React, { useState } from "react";
-import { Segment, Container, Header, Grid, Icon } from "semantic-ui-react";
-import Link from "next/link";
+import { Segment, Container, Header } from "semantic-ui-react";
 import style from "../../static/styles/style.scss";
 import InTheaterBox from "./movies/inTheater";
 import PopularMovies from "./movies/popular";
-import SearchBar from "./ui/searchBar";
 
 const app = () => {
-  const [isSearched, setIsSearched] = useState(false);
-
   return (
     <Container className={style.App}>
-      <Segment basic>
-        <Grid>
-          <Grid.Row>
-            <Grid.Column width="4" />
-            <Grid.Column width="8">
-              <SearchBar setIsSearched={setIsSearched} />
-            </Grid.Column>
-            <Grid.Column width="4" textAlign="left">
-              {isSearched ? (
-                <Link
-                  href={{
-                    pathname: "/movies",
-                    query: { key: "searched" }
-                  }}
-                  as="/movies#searched">
-                  <a style={{ cursor: "pointer" }}>
-                    Show All search results <Icon name="arrow right" />
-                  </a>
-                </Link>
-              ) : null}
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Segment>
       <Segment attached color="green">
         <InTheaterBox />
       </Segment>
--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,12 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import Link from "next/link";
 import Menubar from "./menu";
-import { Container } from "semantic-ui-react";
+import { Container, Segment, Grid, Icon, Divider } from "semantic-ui-react";
+import SearchBar from "../ui/searchBar";
 
 const layout = props => {
+  const [searchQuery, setSearchQuery] = useState(null);
   return (
     <div>
       <Menubar />
+      <Segment attached>
+        <Grid>
+          <Grid.Row>
+            <Grid.Column width="4" />
+            <Grid.Column width="8" textAlign="center">
+              <SearchBar setSearchQuery={setSearchQuery} />
+            </Grid.Column>
+            <Grid.Column width="4" textAlign="left">
+              {searchQuery ? (
+                <Link
+                  href={{
+                    pathname: "/movies",
+                    query: { searched: searchQuery }
+                  }}
+                  as={`/movies/searched/${searchQuery}`}>
+                  <a style={{ cursor: "pointer" }}>
+                    Show All search results <Icon name="arrow right" />
+                  </a>
+                </Link>
+              ) : null}
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+      <Divider hidden />
       <Container>{props.children}</Container>
+      <Segment attached>
+        <h1>This is footer</h1>
+      </Segment>
     </div>
   );
 };
