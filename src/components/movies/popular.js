cfcfb64a00dd5a9850e58f7d7b8cf8da305722c1
pskanade <k.pranav.apk@gmail.com>
2019-09-07 19:44:23 +0530

    corrected the component path

--- src/components/movies/popular.js
+++ src/components/movies/popular.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { getPopularMovies } from "../../api/movies";
-import Listing from "./listing";
+import Listing from "./catalog/listing";
 
 class PopularMovies extends Component {
   state = {
