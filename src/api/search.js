45eaa115925346d53e3578b2002ff9378ed0c513
pskanade <k.pranav.apk@gmail.com>
2019-09-07 16:51:19 +0530

    Added api reqest function to search movies

---
+++ src/api/search.js
@@ -0,0 +1,20 @@
+import { BASEAPI, APIKEY } from "../../global.config";
+import { fetchData } from "./generic";
+
+// Here will only assemble the URL addresses to get the desired data
+
+export const getMovieSearch = async (
+  searchQueryText,
+  storeSearchResults,
+  pageNum = 1
+) => {
+  const APIENDPOINT = "/search/movie";
+  const queryString = `?api_key=${APIKEY}&language=en-US&query=${searchQueryText}&page=${pageNum}&include_adult=false`;
+  const URL = `${BASEAPI}${APIENDPOINT}${queryString}`;
+  try {
+    const data = await fetchData(URL);
+    storeSearchResults(data);
+  } catch (err) {
+    console.log("An error has occured when fetching search results");
+  }
+};
