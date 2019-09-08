546b582ecd59f5401bbc6db0798d31366428f501
pskanade <k.pranav.apk@gmail.com>
2019-09-08 09:39:28 +0530

    moved api key to separate file

--- global.config.js
+++ global.config.js
@@ -1,2 +1,1 @@
-export const APIKEY = "c498daff9107b383d03751205f9969f3";
 export const BASEAPI = "https://api.themoviedb.org/3";
--- src/api/search.js
+++ src/api/search.js
@@ -1,4 +1,5 @@
-import { BASEAPI, APIKEY } from "../../global.config";
+import { BASEAPI } from "../../global.config";
+import { APIKEY } from "../../apiKey.tmdb";
 import { fetchData } from "./generic";
 
 // Here will only assemble the URL addresses to get the desired data
